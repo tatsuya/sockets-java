b5d585c951854d54d5e7df62cd3a253a6e4e3c6d
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2017-12-28 19:39:50 +0900

    initial commit

---
+++ src/main/java/com/tatsuyaoiw/EchoClient.java
@@ -0,0 +1,40 @@
+package com.tatsuyaoiw;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import static java.lang.String.format;
+
+public class EchoClient {
+
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.err.println("Usage: java EchoClient <host name> <port number>");
+            System.exit(1);
+        }
+
+        String hostName = args[0];
+        int portNumber = Integer.parseInt(args[1]);
+
+        try (Socket echoSocket = new Socket(hostName, portNumber);
+             PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+             BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))) {
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+                System.out.println(format("echo: %s", in.readLine()));
+            }
+        } catch (UnknownHostException e) {
+            System.err.println(format("Don't know about host %s", hostName));
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println(format("Couldn't get I/O for the connection to %s", hostName));
+            System.exit(1);
+        }
+    }
+}
---
+++ src/main/java/com/tatsuyaoiw/EchoServer.java
@@ -0,0 +1,36 @@
+package com.tatsuyaoiw;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.String.format;
+
+public class EchoServer {
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Usage: java EchoServer <port number>");
+            System.exit(1);
+        }
+
+        int portNumber = parseInt(args[0]);
+
+        try (ServerSocket serverSocket = new ServerSocket(parseInt(args[0]));
+             Socket clientSocket = serverSocket.accept();
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                out.println(inputLine);
+            }
+        } catch (IOException e) {
+            System.out.println(format("Exception caught when trying to listen on port %d or listening for a connection", portNumber));
+            System.out.println(e.getMessage());
+        }
+    }
+}
